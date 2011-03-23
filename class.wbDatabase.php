6b3e244964215c42ff956dbfe21770aef27a8925
Bianka Martinovic <github.20.cerberus@spamgourmet.com>
2011-03-23 12:50:37 +0100

    * new static method interpolateQuery() for debugging purposes

--- class.wbDatabase.php
+++ class.wbDatabase.php
@@ -61,5 +61,31 @@ class wbDatabase {
     // no cloning!
     private function __clone() {}
 
+    /**
+     * Replaces any parameter placeholders in a query with the value of that
+     * parameter. Useful for debugging. Assumes anonymous parameters from
+     * $params are are in the same order as specified in $query
+     *
+     * Source: http://stackoverflow.com/questions/210564/pdo-prepared-statements
+     *
+     * @param string $query The sql query with parameter placeholders
+     * @param array $params The array of substitution parameters
+     * @return string The interpolated query
+     */
+    public static function interpolateQuery($query, $params) {
+        $keys = array();
+        # build a regular expression for each parameter
+        foreach ($params as $key => $value) {
+            if (is_string($key)) {
+                $keys[] = '/:'.$key.'/';
+            } else {
+                $keys[] = '/[?]/';
+            }
+        }
+        $query = preg_replace($keys, $params, $query, 1, $count);
+        return $query;
+    }
+
+
 
 }
