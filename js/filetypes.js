6e8ffc87eafb5af5481559e7c9e4cc32a4413b52
Bianka Martinovic <github.20.cerberus@spamgourmet.com>
2012-05-16 10:48:36 +0200

    fix: case insensitive check

--- js/filetypes.js
+++ js/filetypes.js
@@ -10,7 +10,7 @@ function TestFileType( fileName, fileTypes ) {
 	dots = fileName.split(".")
 	//get the part AFTER the LAST period.
 	fileType = dots[dots.length-1];
-	if ( fileTypes.join("#").indexOf(fileType) == -1 ) {
+	if ( fileTypes.join("#").toLowerCase().indexOf(fileType.toLowerCase()) == -1 ) {
 		alert("Please only upload files that end in types: \n\n." + (fileTypes.join(" .")) + "\n\nPlease select a new file and try again.");
 	}
 }
